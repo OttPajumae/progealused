50bca7c16befcc6c4ecb92d4c7c6360b0c6f0e82
OttPajumae <92164267+OttPajumae@users.noreply.github.com>
2021-10-08 16:35:49 +0300

    Update checkTemp.js

--- homework/checkTemp.js
+++ homework/checkTemp.js
@@ -1,9 +1,16 @@
+let checktemp = prompt()
 if (checktemp > 0 && checktemp < 20) 
     console.log ("Siin on liiga külm")
-    return -1
 else if (checktemp > 21 && checktemp < 40)
-    console.log ("Siin on paras")
-    return 0 
+    console.log ("Siin on paras")   
 else if (checktemp > 41 && checktemp < 60)
     console.log ("Palun tõmmake temperatuuri alla!!!!")
-    return 1
+    
+function checkTemp(checkt){
+if (checkt > 0 && checkt < 20)    
+    return -1;
+else if (checkt > 21 && checkt < 40)    
+    return 0; 
+else if (checkt > 41 && checkt < 60)
+    return 1;
+}
