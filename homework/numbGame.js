5be2c39a463f2e4605d9ae2ae4d095cc06d46277
OttPajumae <92164267+OttPajumae@users.noreply.github.com>
2021-10-14 17:38:11 +0300

    Create numbGame.js

---
+++ homework/numbGame.js
@@ -0,0 +1,58 @@
+let proovid = 3 
+while (proovid > 0) {
+    console.log (proovid + " katset kokku")
+    proovid -= 1
+    
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+    let tulem = getRandomInt(10)
+    if (prompt() == tulem) {
+        console.log("correct!")
+        break
+    }    
+    else 
+        console.log ("sorry, but the correct awnser was " + tulem)
+        
+       
+    if (proovid >= 2)
+        console.log("Sul on 2 proovi veel")
+    if (proovid <= 1)
+        console.log ("Sul on 1 proov veel")
+    if (proovid == 0 ){
+        console.log ("Mäng on läbi")
+        break      
+    }         
+}
+
+SIIS KUI ON AINULT 1 ARVUGA!!!!! 
+  
+let proovid = 3 
+while (proovid > 0) {
+    console.log (proovid + " katset kokku")
+    proovid -= 1
+function arv (min, max) {
+return Math.floor(Math.floor() * (max-min))+ min 
+    }   
+
+var katseid = 3 
+let tegelikarv = arv (0, 11)
+console.log (tegelikarv + " on õige vastus")
+ 
+    if (prompt() == tulem) {
+        console.log("correct!")
+        break
+    }    
+    else 
+        console.log ("sorry, but the correct awnser was " + tulem)
+        
+       
+    if (proovid >= 2)
+        console.log("Sul on 2 proovi veel")
+    if (proovid <= 1)
+        console.log ("Sul on 1 proov veel")
+    if (proovid == 0 ){
+        console.log ("Mäng on läbi")
+        break      
+    }         
+}    
